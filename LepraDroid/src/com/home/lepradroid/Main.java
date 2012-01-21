f0f7a247d38c8f7ce1ea5098e8392db41329581f
roman <roman.abdulmanov@gmail.com>
2012-01-21 20:38:54 +0400

    first commit

---
+++ LepraDroid/src/com/home/lepradroid/Main.java
@@ -0,0 +1,20 @@
+package com.home.lepradroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    public void func()
+    {
+        
+    }
+}
