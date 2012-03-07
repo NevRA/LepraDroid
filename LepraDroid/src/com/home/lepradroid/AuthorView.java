44b0af520865018db9b1a50027eacc4f4bce5889
gobozov <gobozov@gmail.com>
2012-03-07 17:47:11 +0400

    author view - plus\minus buttons

--- LepraDroid/src/com/home/lepradroid/AuthorView.java
+++ LepraDroid/src/com/home/lepradroid/AuthorView.java
@@ -3,77 +3,77 @@ package com.home.lepradroid;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.*;
 
 import com.home.lepradroid.base.BaseView;
 import com.home.lepradroid.interfaces.AuthorUpdateListener;
 import com.home.lepradroid.listenersworker.ListenersWorker;
 import com.home.lepradroid.objects.Author;
+import com.home.lepradroid.settings.SettingsWorker;
 import com.home.lepradroid.utils.ImageLoader;
 
-public class AuthorView extends BaseView implements AuthorUpdateListener
-{
-    private String              userName;
-    private RelativeLayout      contentLayout;
-    private TextView            name;
-    private TextView            ego;
-    private TextView            rating;
-    private ImageView           userPic;
-    private ProgressBar         progress;
-    private ImageLoader         imageLoader;
-    
-    public AuthorView(Context context, String userName)
-    {
+public class AuthorView extends BaseView implements AuthorUpdateListener {
+    private String userName;
+    private RelativeLayout contentLayout;
+    private TextView name;
+    private TextView ego;
+    private TextView rating;
+    private ImageView userPic;
+    private ProgressBar progress;
+    private ImageLoader imageLoader;
+    private LinearLayout buttonsLayout;
+    private Button plus;
+    private Button minus;
+
+    public AuthorView(Context context, String userName) {
         super(context);
-        
+
         this.userName = userName;
-        
+
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        if (inflater != null)
-        {
+        if (inflater != null) {
             contentView = inflater.inflate(R.layout.author_view, null);
         }
-        
+
         init();
     }
-    
-    public void setUserName(String userName)
-    {
+
+    public void setUserName(String userName) {
         this.userName = userName;
     }
 
-    private void init()
-    {
-        imageLoader=new ImageLoader(LepraDroidApplication.getInstance());
-        
+    private void init() {
+        imageLoader = new ImageLoader(LepraDroidApplication.getInstance());
+
         contentLayout = (RelativeLayout) contentView.findViewById(R.id.content);
         name = (TextView) contentView.findViewById(R.id.name);
         ego = (TextView) contentView.findViewById(R.id.userego);
         rating = (TextView) contentView.findViewById(R.id.rating);
         userPic = (ImageView) contentView.findViewById(R.id.image);
         progress = (ProgressBar) contentView.findViewById(R.id.progress);
+        buttonsLayout = (LinearLayout) contentView.findViewById(R.id.buttons);
+        plus = (Button) contentView.findViewById(R.id.plus);
+        minus = (Button) contentView.findViewById(R.id.minus);
     }
 
     @Override
-    public void OnExit()
-    {
+    public void OnExit() {
         ListenersWorker.Instance().unregisterListener(this);
     }
 
     @Override
-    public void OnAuthorUpdate(String userName, Author data)
-    {
-        if(!this.userName.equals(userName)) return;
-        
+    public void OnAuthorUpdate(String userName, Author data) {
+        if (!this.userName.equals(userName)) return;
+
         progress.setVisibility(View.GONE);
         contentLayout.setVisibility(View.VISIBLE);
         
-        if(data == null) return;
-        
+        if (!SettingsWorker.Instance().loadUserName().equals(userName))
+            buttonsLayout.setVisibility(View.VISIBLE);
+
+        if (data == null) return;
+
         name.setText(data.Name);
         ego.setText(data.Ego);
         rating.setText(data.Rating.toString());
@@ -82,11 +82,11 @@ public class AuthorView extends BaseView implements AuthorUpdateListener
     }
 
     @Override
-    public void OnAuthorUpdateBegin(String userName)
-    {
-        if(!this.userName.equals(userName)) return;
-        
+    public void OnAuthorUpdateBegin(String userName) {
+        if (!this.userName.equals(userName)) return;
+
         progress.setVisibility(View.VISIBLE);
         contentLayout.setVisibility(View.GONE);
+        buttonsLayout.setVisibility(View.GONE);
     }
 }
