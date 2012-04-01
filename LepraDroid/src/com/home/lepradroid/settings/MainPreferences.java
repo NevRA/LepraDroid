afbfeb84e5f9a05a44ce019094034272bc54f545
NevRA <roman.abdulmanov@gmail.com>
2012-04-01 23:23:09 +0400

    Added history of changes to settings screen

--- LepraDroid/src/com/home/lepradroid/settings/MainPreferences.java
+++ LepraDroid/src/com/home/lepradroid/settings/MainPreferences.java
@@ -4,11 +4,17 @@ import com.home.lepradroid.AuthorScreen;
 import com.home.lepradroid.R;
 import com.home.lepradroid.utils.Utils;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.webkit.WebView;
 
 public class MainPreferences extends PreferenceActivity
 {
@@ -30,5 +36,40 @@ public class MainPreferences extends PreferenceActivity
                 return true;
             }
         });
+        
+        final Preference history = (Preference) getPreferenceManager().findPreference(Utils.getString(R.string.MainSettings_HistoryId));
+        history.setOnPreferenceClickListener(new OnPreferenceClickListener() 
+        {
+            public boolean onPreferenceClick(Preference preference) 
+            {
+                LayoutInflater inflater = LayoutInflater.from(MainPreferences.this);
+
+                View alertDialogView = inflater.inflate(R.layout.history_view, null);
+
+                WebView webView = (WebView) alertDialogView.findViewById(R.id.DialogWebView);
+                webView.getSettings().setDefaultFontSize(10);
+                webView.loadUrl("file:///android_asset/history.html");
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainPreferences.this);
+                builder.setView(alertDialogView);
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() 
+                {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                }).show();
+                
+                return true;
+            }
+        });
+        
+        final Preference version = (Preference) getPreferenceManager().findPreference(Utils.getString(R.string.MainSettings_VersionId));
+        try
+        {
+            version.setSummary(getPackageManager().getPackageInfo(getPackageName(), 0).versionName);
+        }
+        catch (NameNotFoundException e)
+        {
+        }
     }
 }
