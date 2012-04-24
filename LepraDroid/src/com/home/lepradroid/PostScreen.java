4c027e3ea1308895394c2fcf834178437bb977fa
NevRA <roman.abdulmanov@gmail.com>
2012-04-25 00:52:15 +0400

    Minor fix

--- LepraDroid/src/com/home/lepradroid/PostScreen.java
+++ LepraDroid/src/com/home/lepradroid/PostScreen.java
@@ -95,13 +95,13 @@ public class PostScreen extends BaseActivity implements ChangeMyStuffListener
             menu.add(0, MENU_COMMENT_NAVIGATE, 0, navigationTurnedOn ? Utils.getString(R.string.Turn_Off_Navigation) : Utils.getString(R.string.Turn_On_Navigation)).setIcon(navigationTurnedOn ? R.drawable.ic_comment_navigation_off : R.drawable.ic_comment_navigation);
             menu.add(0, MENU_RELOAD, 1, Utils.getString(R.string.Reload_Menu)).setIcon(R.drawable.ic_reload);
             menu.add(0, MENU_ADD_COMMENT, 2, Utils.getString(R.string.Comment_Menu)).setIcon(R.drawable.ic_add_comment);
-            if(!groupId.equals(Commons.INBOX_POSTS_ID))
+            /*if(!groupId.equals(Commons.INBOX_POSTS_ID))
             {
                 if(Utils.isAlreadyInMyStuff(post.getPid()))
                     menu.add(0, MENU_DEL_STUFF, 3, Utils.getString(R.string.Del_Stuff_Menu)).setIcon(R.drawable.ic_del_stuff);
                 else
                     menu.add(0, MENU_ADD_STUFF, 3, Utils.getString(R.string.Add_Stuff_Menu)).setIcon(R.drawable.ic_add_stuff);
-            }
+            }*/
             break;
         default:
             menu.add(0, MENU_RELOAD, 0, Utils.getString(R.string.Reload_Menu)).setIcon(R.drawable.ic_reload);
